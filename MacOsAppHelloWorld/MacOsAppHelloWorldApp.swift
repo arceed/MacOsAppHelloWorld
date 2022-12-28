6a6f13f29fe436d6153537f895f9da36c51c753e
Hanwell <hanwell2002@gmail.com>
2022-12-28 18:11:48 +0300

    Initial Commit

---
+++ MacOsAppHelloWorld/MacOsAppHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOsAppHelloWorldApp.swift
+//  MacOsAppHelloWorld
+//
+//  Created by Admin on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MacOsAppHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
